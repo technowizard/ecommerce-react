d7cb0cd3301a4553c3ba0a3f8e4d844e7a10d390
technowizard <technowizard17@gmail.com>
2020-09-20 16:10:56 +0700

    added stripe integration, updating favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -34,9 +35,14 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
 
-    <div className="total">
-      <span>TOTAL: ${total}</span>
+    <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      *Please use the following credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: XX/XX (use any date) - CVV: any three digits
+      number (i.e. 123){' '}
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
